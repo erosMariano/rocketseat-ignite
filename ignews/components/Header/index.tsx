c62e82f7c2ecfef4af79c4df4f719a9c03b6bb49
erosMariano <erosmariano1@gmail.com>
2022-01-02 16:42:39 -0300

    button signin estatico + header concluido

--- ignews/components/Header/index.tsx
+++ ignews/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./styles.module.scss";
+import SignInButton from "../SignInButton";
 
 function Header() {
 	return (
@@ -17,6 +18,7 @@ function Header() {
 					<a className={styles.active}>Home</a>
 					<a>Posts</a>
 				</nav>
+				<SignInButton />
 			</div>
 		</header>
 	);

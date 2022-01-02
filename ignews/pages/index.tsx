3137920cfa6adc9b06f161a62f875078df6f3705
erosMariano <erosmariano1@gmail.com>
2022-01-02 19:26:22 -0300

    hero component add

--- ignews/pages/index.tsx
+++ ignews/pages/index.tsx
@@ -1,12 +1,36 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./home.module.scss";
 
 const Home: NextPage = () => {
-  return (
-    <>Eros</>
-  )
-}
+	return (
+		<>
+			<Head>
+				<title>Home | ig.news</title>
+			</Head>
 
-export default Home
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>🤙 Hey, welcome</span>
+					<h1>
+						News about the <span>React</span> world.
+					</h1>
+					<p>
+						Get access to all the publications <br />
+						<span>for $9.90 month</span>
+					</p>
+				</section>
+
+				<Image
+					src="/images/avatar.svg"
+					alt="girl coding"
+					width="336"
+					height="521"
+				/>
+			</main>
+		</>
+	);
+};
+
+export default Home;
